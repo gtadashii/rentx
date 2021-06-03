966b0b24289e12a1afb89d10a0515ec1c06648a2
Guilherme Tadashii <guilhermetadashii.dev@gmail.com>
2021-06-03 13:57:25 -0300

    Added session route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+import { authenticateRoutes } from "./authenticate.routes";
 import { categoriesRoutes } from "./categories.routes";
 import { speficationRoutes } from "./specifications.routes";
 import { usersRoutes } from "./users.routes";
@@ -9,5 +10,6 @@ const router = Router();
 router.use("/categories", categoriesRoutes);
 router.use("/specifications", speficationRoutes);
 router.use("/users", usersRoutes);
+router.use(authenticateRoutes);
 
 export { router };

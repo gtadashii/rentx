cebdecdb43c38ce42dc3e53629d753cecb155940
Guilherme Tadashii <guilhermetadashii.dev@gmail.com>
2021-06-09 16:47:48 -0300

    Added Create Car testing

---
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -0,0 +1,66 @@
+import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarRepositoryInMemory";
+import { AppError } from "@shared/errors/AppError";
+
+import { CreateCarUseCase } from "./CreateCarUseCase";
+
+let createCarUseCase: CreateCarUseCase;
+let carsRepositoryInMemory: CarsRepositoryInMemory;
+
+describe("Create Car", () => {
+  beforeEach(() => {
+    carsRepositoryInMemory = new CarsRepositoryInMemory();
+    createCarUseCase = new CreateCarUseCase(carsRepositoryInMemory);
+  });
+
+  it("Should be able to create a new car", async () => {
+    const car = await createCarUseCase.execute({
+      name: "Car Name",
+      description: "Car Description",
+      daily_rate: 100,
+      license_plate: "ABC-1234",
+      fine_amount: 60,
+      brand: "Car Brand",
+      category_id: "category",
+    });
+
+    expect(car).toHaveProperty("id");
+  });
+
+  it("Should not be able to create a car with existing license plate", () => {
+    expect(async () => {
+      await createCarUseCase.execute({
+        name: "Car1",
+        description: "Car Description",
+        daily_rate: 100,
+        license_plate: "ABC-1234",
+        fine_amount: 60,
+        brand: "Car Brand",
+        category_id: "category",
+      });
+
+      await createCarUseCase.execute({
+        name: "Car2",
+        description: "Car Description",
+        daily_rate: 100,
+        license_plate: "ABC-1234",
+        fine_amount: 60,
+        brand: "Car Brand",
+        category_id: "category",
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("Should be able to create a car with avaliable as true by default", async () => {
+    const car = await createCarUseCase.execute({
+      name: "Car Available",
+      description: "Car Description",
+      daily_rate: 100,
+      license_plate: "ABD-1234",
+      fine_amount: 60,
+      brand: "Car Brand",
+      category_id: "category",
+    });
+
+    expect(car.available).toBe(true);
+  });
+});

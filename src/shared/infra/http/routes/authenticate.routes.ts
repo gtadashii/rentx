4a0581316da43eb3cba70853c1a708f7254d8ad9
Guilherme Tadashii <guilhermetadashii.dev@gmail.com>
2021-06-27 15:25:37 -0300

    Implemented refresh token feature

--- src/shared/infra/http/routes/authenticate.routes.ts
+++ src/shared/infra/http/routes/authenticate.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
 import { AuthenticateUserController } from "@modules/accounts/useCases/authenticateUser/AuthenticateUserController";
+import { RefreshTokenController } from "@modules/accounts/useCases/refreshToken/RefreshTokenController";
 
 const authenticateRoutes = Router();
 
 const authenticateUserController = new AuthenticateUserController();
+const refreshTokenControler = new RefreshTokenController();
 
 authenticateRoutes.post("/sessions", authenticateUserController.handle);
+authenticateRoutes.post("/refresh_token", refreshTokenControler.handle);
 
 export { authenticateRoutes };
